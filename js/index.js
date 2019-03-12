8d049661c3ab85cc445add4c5f58c5ecade4a18f
Hui-LingPhillips <huilinglou@gmail.com>
2019-03-12 19:45:11 +0000

    fin

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// web page is rendered in the browser before any
+//jQuery code executes.
+$(document).ready(() => {
+
+//navbar dropdowns
+
+  $('#CafeDropdown').on('click', () => {
+    $('#cafe-dd-menu').show();
+    })
+
+  $('#TeahouseDropdown').on('click', () => {
+    $('#teahouse-dd-menu').show();
+  })
+
+
+  $('#carouselIndicators').carousel({
+    interval: 2000
+  })
+
+  $('.accordian').collapse('toggle')
+
+  $().button('toggle')
+
+})
